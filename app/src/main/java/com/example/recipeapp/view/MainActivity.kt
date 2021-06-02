918004e41757abd701d664d487eb725baaad39c5
Danil Berdnikov <danil.b0101@bk.ru>
2021-06-02 21:18:46 +0700

    Fix MainActivity default layout

--- app/src/main/java/com/example/recipeapp/view/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/view/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.recipeapp.R
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_description)
+        setContentView(R.layout.activity_main)
     }
 }
